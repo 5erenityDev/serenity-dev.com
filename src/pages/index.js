240b8964f5b16f9c4b36fc1c6239e6fc99dce5d5
Sean Blankenship <seanblankenship2002@gmail.com>
2025-01-18 16:02:38 -0500

    Initial Site Data
    Pushing essential data for a Gatsby site

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+    <main>
+      <h1>SerenityDev</h1>
+      <p>Indie Game Developer</p>
+      <p>Site (UNDER DEVELOPMENT) built with GatsbyJS and deployed with Netlify</p>
+    </main>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Sean Blankenship</title>
